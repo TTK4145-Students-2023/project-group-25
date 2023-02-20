d9b55e79754039137a2af564fedd1ceb204be360
Martin <martin.siggerud@gmail.com>
2023-02-20 14:54:03 +0100

    ip adress test

---
+++ MasterSlaveAssigner/MasterSlaveAssigner.go
@@ -0,0 +1,32 @@
+package MasterSlaveAssigner
+package main
+
+import (
+    "log"
+    "net"
+    "strings"
+	"fmt"
+)
+
+// Get preferred outbound ip of this machine
+func GetOutboundIP() net.IP {
+    conn, err := net.Dial("udp", "8.8.8.8:80")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer conn.Close()
+
+    localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+    return localAddr.IP
+}
+
+func MasterSlaveAssigner (){
+	selfIPaddr = GetOutboundIP
+	print.fmt(selfIPaddr)
+}
+
+func main(){
+	go MasterSlaveAssigner
+}
+
