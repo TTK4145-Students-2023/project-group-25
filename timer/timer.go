360e18c291d3de5863f5424085a9ec9e176e8675
Fredrik Mo <fjmo@stud.ntnu.no>
2023-02-20 11:08:02 +0100

    redo timer

--- timer/timer.go
+++ timer/timer.go
@@ -5,25 +5,27 @@ import (
 )
 
 var (
-	setAlarmTime = make(chan time.Time)
-	getAlarmTime = make(chan time.Time)
+	startTimer = make(chan time.Duration)
+	killTimer  = make(chan bool)
 )
 
-func Timer() {
-	alarmTime := time.Now().Local()
+func TimerMain(timeout chan<- bool) {
+
 	for {
+		s := <-startTimer
+		alarm := time.After(time.Second * s)
 		select {
-		case alarmTime = <-setAlarmTime:
-		case getAlarmTime <- alarmTime:
+		case <-alarm:
+			timeout <- true
+		case <-killTimer:
 		}
 	}
 }
 
-func SetTimer(seconds int) {
-	setAlarmTime <- time.Now().Local().Add(time.Second * time.Duration(seconds))
+func TimerStart(seconds time.Duration) {
+	startTimer <- seconds
 }
 
-func TimeLeft() bool {
-	difference := (<-getAlarmTime).Sub(time.Now().Local())
-	return difference >= 0
+func TimerKill() {
+	killTimer <- true
 }
