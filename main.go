4e399d3a4f6e3121a89bb211728d64da1a31def3
Fredrik Mo <fjmo@stud.ntnu.no>
2023-03-01 16:56:51 +0100

    namechanges

--- main.go
+++ main.go
@@ -7,16 +7,16 @@ import (
 )
 
 var (
-	floor_hallRequest      = make(chan [elevfsm.N_FLOORS][2]bool)
-	floor_cabButtonEvent   = make(chan elevio.ButtonEvent)
-	handler_ordersExecuted = make(chan []elevio.ButtonEvent)
-	drv_buttons            = make(chan elevio.ButtonEvent)
-	drv_floors             = make(chan int)
-	drv_obstr              = make(chan bool)
-	elev_data              = make(chan elevfsm.ElevatorData)
+	floor_hallRequest          = make(chan [elevfsm.N_FLOORS][2]bool)
+	floor_cabButtonEvent       = make(chan elevio.ButtonEvent)
+	handler_hallOrdersExecuted = make(chan []elevio.ButtonEvent)
+	drv_buttons                = make(chan elevio.ButtonEvent)
+	drv_floors                 = make(chan int)
+	drv_obstr                  = make(chan bool)
+	elev_data                  = make(chan elevfsm.ElevatorData)
 )
 
-func orderHandler(buttonPress chan elevio.ButtonEvent, orderClear chan []elevio.ButtonEvent, hallOrder chan [elevfsm.N_FLOORS][2]bool, floor_cabButtonEvent chan elevio.ButtonEvent) {
+func orderHandler(buttonPress chan elevio.ButtonEvent, handler_hallOrdersExecuted chan []elevio.ButtonEvent, hallOrder chan [elevfsm.N_FLOORS][2]bool, floor_cabButtonEvent chan elevio.ButtonEvent) {
 	elev_hallOrder := [elevfsm.N_FLOORS][2]bool{}
 
 	for {
@@ -30,11 +30,11 @@ func orderHandler(buttonPress chan elevio.ButtonEvent, orderClear chan []elevio.
 				hallOrder <- elev_hallOrder
 			}
 
-		case clearEvent := <-orderClear:
-			for i := 0; i < len(clearEvent); i++ {
-				if clearEvent[i].Button != elevio.BT_Cab {
-					elev_hallOrder[clearEvent[i].Floor][clearEvent[i].Button] = false
-					elevio.SetButtonLamp(clearEvent[i].Button, clearEvent[i].Floor, false)
+		case hallOrdersExecuted := <-handler_hallOrdersExecuted:
+			for i := 0; i < len(hallOrdersExecuted); i++ {
+				if hallOrdersExecuted[i].Button != elevio.BT_Cab {
+					elev_hallOrder[hallOrdersExecuted[i].Floor][hallOrdersExecuted[i].Button] = false
+					elevio.SetButtonLamp(hallOrdersExecuted[i].Button, hallOrdersExecuted[i].Floor, false)
 				}
 			}
 			hallOrder <- elev_hallOrder
@@ -48,11 +48,11 @@ func main() {
 	go elevio.PollFloorSensor(drv_floors)
 	go elevio.PollButtons(drv_buttons)
 	go elevio.PollObstructionSwitch(drv_obstr)
-	go orderHandler(drv_buttons, handler_ordersExecuted, floor_hallRequest, floor_cabButtonEvent)
+	go orderHandler(drv_buttons, handler_hallOrdersExecuted, floor_hallRequest, floor_cabButtonEvent)
 
 	time.Sleep(time.Millisecond * 40)
 
-	go elevfsm.FSM(floor_hallRequest, floor_cabButtonEvent, drv_floors, drv_obstr, elev_data, handler_ordersExecuted)
+	go elevfsm.FSM(floor_hallRequest, floor_cabButtonEvent, drv_floors, drv_obstr, elev_data, handler_hallOrdersExecuted)
 
 	for {
 		<-elev_data
