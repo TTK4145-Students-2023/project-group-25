d9b9114bd700555d8d7315772963aeb2b6a962fe
eveneha <eveneha@stud.ntnu.no>
2023-03-13 13:49:12 +0100

    always broadcasting with sample rate 40ms

--- Network/P2Pntw/P2Pntw.go
+++ Network/P2Pntw/P2Pntw.go
@@ -3,23 +3,56 @@ package P2P
 import (
 	"project/Network/Utilities/bcast"
 	dt "project/commonDataTypes"
+	"reflect"
+	"time"
 )
 
 func P2Pntw(
-	// Receive channels
-	allElevData_fromDist chan dt.AllElevDataJSON_withID,
-	ReqStateMatrix_fromOrderHandler chan dt.RequestStateMatrix,
-
-	// Sending channels
-	allElevData_toDist chan dt.AllElevDataJSON_withID,
-	ReqStateMatrix_toOrderHandler chan dt.RequestStateMatrix,
+	localWorldViewChan <-chan dt.AllElevDataJSON_withID,
+	localRequestStateMatrixChan <-chan dt.RequestStateMatrix,
+	externalWorldViewChan chan<- dt.AllElevDataJSON_withID,
+	externalRequestStateMatrixChan chan<- dt.RequestStateMatrix,
 
 ) {
+	var (
+		transmittWorldVeiw          = make(chan<- dt.AllElevDataJSON_withID)
+		transmittRequestStateMatrix = make(chan<- dt.RequestStateMatrix)
+		receiveWorldView            = make(<-chan dt.AllElevDataJSON_withID)
+		receiveRequestStateMatrix   = make(<-chan dt.RequestStateMatrix)
+	)
+
+	localWorldView := dt.AllElevDataJSON_withID{}
+	localRequestStateMatrix := dt.RequestStateMatrix{}
+
+	externalWorldView := dt.AllElevDataJSON_withID{}
+	externalRequestStateMatrix := dt.RequestStateMatrix{}
+
 	// Receive from NTW
-	go bcast.Receiver(15647, allElevData_toDist)
-	go bcast.Receiver(15648, ReqStateMatrix_toOrderHandler)
+	go bcast.Receiver(15647, receiveWorldView)
+	go bcast.Receiver(15648, receiveRequestStateMatrix)
 
 	// Send to NTW
-	go bcast.Transmitter(15647, allElevData_fromDist)
-	go bcast.Transmitter(15648, ReqStateMatrix_fromOrderHandler)
+	go bcast.Transmitter(15647, transmittWorldVeiw)
+	go bcast.Transmitter(15648, transmittRequestStateMatrix)
+
+	for {
+		select {
+		case localRequestStateMatrix = <-localRequestStateMatrixChan:
+		case localWorldView = <-localWorldViewChan:
+		case newRequestStateMatrix := <-receiveRequestStateMatrix:
+			if !reflect.DeepEqual(newRequestStateMatrix, externalRequestStateMatrix) {
+				externalRequestStateMatrix = newRequestStateMatrix
+				externalRequestStateMatrixChan <- externalRequestStateMatrix
+			}
+		case newWorldView := <-receiveWorldView:
+			if !reflect.DeepEqual(newWorldView, externalWorldView) {
+				externalWorldView = newWorldView
+				externalWorldViewChan <- externalWorldView
+			}
+		default:
+			time.Sleep(time.Millisecond * 40)
+		}
+		transmittWorldVeiw <- localWorldView
+		transmittRequestStateMatrix <- localRequestStateMatrix
+	}
 }
--- Network/masterSlaveNTW/masterSlaveNTW.go
+++ Network/masterSlaveNTW/masterSlaveNTW.go
@@ -1,11 +1,12 @@
 package masterSlaveNTW
 
 import (
-	"fmt"
 	"project/Network/Utilities/bcast"
 	"project/Network/Utilities/localip"
 	"project/Network/Utilities/peers"
 	dt "project/commonDataTypes"
+	"reflect"
+	"time"
 )
 
 // datatypes
@@ -13,52 +14,48 @@ type AssignedOrders map[string][]bool
 
 func MasterSlaveNTW(
 
-	// Receive channels
 	peerUpdate_MS chan peers.PeerUpdate,
-	inputOrders_fromAss chan []byte,
-	// Sending channels
-
-	outputOrders_toAss chan []byte,
+	localOrdersChan <-chan []byte,
+	externalOrdersChan chan<- []byte,
 	masterOrSlave chan dt.MasterSlaveRole,
 ) {
 	var (
-		inputOrders_fromNTW = make(chan []byte)
-		outputOrders_toNTW  = make(chan []byte)
+		receiveOrdersChan   = make(chan []byte)
+		transmittOrdersChan = make(chan []byte)
 	)
 
 	localIP, _ := localip.LocalIP()
 
-	// Receive from NTW
-	go bcast.Receiver(15640, inputOrders_fromNTW)
+	go bcast.Receiver(15640, receiveOrdersChan)
+	go bcast.Transmitter(15640, transmittOrdersChan)
 
-	// Send on NTW
-	go bcast.Transmitter(15640, outputOrders_toNTW)
-
-	Local_MS_role := dt.MS_Slave
+	local_MS_role := dt.MS_Slave
+	localOrders := []byte{}
+	externalOrders := []byte{}
 
 	for {
 		select {
 		case peerList := <-peerUpdate_MS:
-			//update local MS_role
-			fmt.Printf("_____PEER LIST______\n  %s\n", peerList)
-			Local_MS_role = MS_Assigner(localIP, peerList.Peers)
-			fmt.Printf("_____MS ROLE______\n  %s\n", Local_MS_role)
-			masterOrSlave <- Local_MS_role
-			fmt.Printf("_____MS IS ASSIGNED______\n  %s\n", Local_MS_role)
-			//send peerlsit to other?
-		case ordersFromNTW := <-inputOrders_fromNTW:
-			switch Local_MS_role {
-			case dt.MS_Master:
-			case dt.MS_Slave:
-				outputOrders_toAss <- ordersFromNTW
-			}
-		case ordersFromAss := <-inputOrders_fromAss:
-			switch Local_MS_role {
-			case dt.MS_Slave:
-			case dt.MS_Master:
-				outputOrders_toNTW <- ordersFromAss
+			local_MS_role = MS_Assigner(localIP, peerList.Peers)
+			masterOrSlave <- local_MS_role
 
+		case localOrders = <-localOrdersChan:
+		case newOrders := <-receiveOrdersChan:
+			if !reflect.DeepEqual(newOrders, externalOrders) {
+				externalOrders = newOrders
+				switch local_MS_role {
+				case dt.MS_Master:
+				case dt.MS_Slave:
+					externalOrdersChan <- externalOrders
+				}
 			}
+		default:
+			time.Sleep(time.Millisecond * 40)
+		}
+		switch local_MS_role {
+		case dt.MS_Slave:
+		case dt.MS_Master:
+			transmittOrdersChan <- localOrders
 		}
 	}
 }
