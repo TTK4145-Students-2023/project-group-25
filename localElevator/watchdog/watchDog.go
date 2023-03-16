5d70d0a4fede5249e1a93e399303c72b34184b96
mariusvaardal <mariusvaardal@gmail.com>
2023-03-16 16:44:22 +0100

    Implementing WDT. Dont know if it works yet

--- localElevator/watchdog/watchDog.go
+++ localElevator/watchdog/watchDog.go
@@ -5,29 +5,41 @@ import (
 	"time"
 )
 
+type WD_Role string
+
 const (
-	watchDogTime = 10 // Time [s] for watchdog
+	watchDogTime         = 10 // Time [s] for watchdog
+	WD_ALIVE     WD_Role = "ALIVE"
+	WD_DEAD      WD_Role = "DEAD"
 )
 
-type ElevState string
-
 func WatchDog(
-	elevData chan dt.ElevDataJSON) {
-	deadOrAliveCh := make(chan string)
+	elevData chan dt.ElevDataJSON,
+	peerTxEnable chan bool) {
 	watchDogTimer := time.NewTimer(watchDogTime * time.Second)
+	watchDogRole := WD_ALIVE
+	elevState := dt.ElevDataJSON{}
 	for {
 		select {
-		case <-elevData:
-			deadOrAlive := <-deadOrAliveCh
-			switch deadOrAlive {
+		case elevState = <-elevData:
+			watchDogTimer.Reset(watchDogTime * time.Second)
+			switch watchDogRole {
 			case "ALIVE":
-				watchDogTimer.Reset(watchDogTime * time.Second)
 			case "DEAD":
-				// Connect to NTW
-				watchDogTimer.Reset(watchDogTime * time.Second)
-				deadOrAliveCh <- "ALIVE"
+				peerTxEnable <- true
+				watchDogRole = "ALIVE"
 			}
 		case <-watchDogTimer.C:
+			switch elevState.Behavior {
+			case "idle":
+				watchDogTimer.Reset(watchDogTime * time.Second)
+			case "moving":
+				peerTxEnable <- false // Disconnect form network (peerTxEnable chan)
+				watchDogRole = "DEAD"
+			case "doorOpen":
+				peerTxEnable <- false // Disconnect form network (peerTxEnable chan)
+				watchDogRole = "DEAD"
+			}
 		}
 	}
 }
