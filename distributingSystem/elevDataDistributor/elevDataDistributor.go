91ca4ffecdd1e6a21b5c70d2f4b8235569237832
Fredrik Johan Stokke Mo <fjmo@stud.ntnu.no>
2023-03-21 15:24:00 +0100

    clean up dataDistributor module

--- distributingSystem/elevDataDistributor/elevDataDistributor.go
+++ distributingSystem/elevDataDistributor/elevDataDistributor.go
@@ -1,7 +1,6 @@
 package elevDataDistributor
 
 import (
-	"fmt"
 	"project/Network/Utilities/bcast"
 	"project/Network/Utilities/peers"
 	dt "project/commonDataTypes"
@@ -12,104 +11,92 @@ import (
 // Statemachine for Distributor
 func DataDistributor(localIP string,
 	localElevDataCh <-chan dt.ElevData,
-	HallOrderArrayCh <-chan [dt.N_FLOORS][2]bool,
+	HallRequestsCh <-chan [dt.N_FLOORS][2]bool,
 	costFuncInputCh chan<- dt.CostFuncInputSlice,
 	peerUpdateCh <-chan peers.PeerUpdate,
 	cabRequestsToElevCh chan<- [dt.N_FLOORS]bool,
 ) {
-	localNodesInfo := map[string]dt.ElevData{}
-	costFuncInputSlice := dt.CostFuncInputSlice{}
-	peerList := peers.PeerUpdate{}
-
+	const dataDistributorPort = 15667
 	var (
-		transmittNodesInfo = make(chan dt.AllNodeInfoWithSenderIP)
-		receiveNodesInfo   = make(chan dt.AllNodeInfoWithSenderIP)
+		transmittCh = make(chan dt.AllNodeInfoWithSenderIP)
+		receiveCh   = make(chan dt.AllNodeInfoWithSenderIP)
+
+		broadCastTimer = time.NewTimer(time.Hour)
+		costFuncTimer  = time.NewTimer(time.Hour)
+		initTimer      = time.NewTimer(2 * time.Second)
+
+		costFuncInputSlice = dt.CostFuncInputSlice{}
+		allElevData        = map[string]dt.ElevData{}
+		peerList           = peers.PeerUpdate{}
 	)
-	go bcast.Receiver(15667, receiveNodesInfo)
-	go bcast.Transmitter(15667, transmittNodesInfo)
 
-	worldViewTimer := time.NewTimer(1)
-	worldViewTimer.Stop()
-	broadCastTimer := time.NewTimer(1)
 	broadCastTimer.Stop()
+	costFuncTimer.Stop()
 
-	initTimer := time.NewTimer(1 * time.Second)
+	go bcast.Transmitter(dataDistributorPort, transmittCh)
+	go bcast.Receiver(dataDistributorPort, receiveCh)
 
-	//Send cabcalls to fsm if cabcall allready exist on network
-	initCabCalls := [dt.N_FLOORS]bool{}
-	for initTimeOut := false; !initTimeOut; {
+initialization:
+	for cabRequests := [dt.N_FLOORS]bool{}; ; {
 		select {
-		case NTWData := <-receiveNodesInfo:
-			fmt.Printf("NodesInfoFromNTW:\n %+v\n\n", NTWData)
-			senderIP := NTWData.SenderIP
-			senderNodesInfo := dt.NodeInfoSliceToMap(NTWData.AllNodeInfo)
+		case receivedData := <-receiveCh:
+			senderIP := receivedData.SenderIP
+			allNodesInfo := receivedData.AllNodeInfo
 
-			localNodesInfo[senderIP] = senderNodesInfo[senderIP]
-
-			if _, valInMap := senderNodesInfo[localIP]; valInMap {
-				for floor, order := range senderNodesInfo[localIP].CabRequests {
-					if order {
-						initCabCalls[floor] = order
+			for _, nodeInfo := range allNodesInfo {
+				if elevData := nodeInfo.Data; nodeInfo.IP == localIP {
+					for floor, order := range elevData.CabRequests {
+						cabRequests[floor] = cabRequests[floor] || order
 					}
+				} else if nodeInfo.IP == senderIP {
+					allElevData[senderIP] = elevData
 				}
-				//send to fsm
-				fmt.Printf("\ncabcalls from NTW: %+v\n", initCabCalls)
 			}
 		case <-initTimer.C:
-			initTimeOut = true
-			cabRequestsToElevCh <- initCabCalls
+			cabRequestsToElevCh <- cabRequests
+			allElevData[localIP] = <-localElevDataCh
+			broadCastTimer.Reset(1)
+			break initialization
 		}
 	}
-
-	fmt.Println("enter normal DD")
 	for {
 		select {
-
 		case peerList = <-peerUpdateCh:
-		case newNodesInfo := <-receiveNodesInfo:
-			senderData := dt.NodeInfoSliceToMap(newNodesInfo.AllNodeInfo)
-			senderIP := newNodesInfo.SenderIP
-			//fmt.Printf("nodesInfo fro NTW: %+v\n", senderData)
-			if localIP != senderIP && !reflect.DeepEqual(senderData[senderIP], localNodesInfo[senderIP]) {
-				localNodesInfo[senderIP] = senderData[senderIP]
-			}
-
-		case elevData := <-localElevDataCh:
-			if _, valInMap := localNodesInfo[localIP]; !valInMap {
-				broadCastTimer.Reset(1)
-			}
-			localNodesInfo[localIP] = elevData
-
-		case orders := <-HallOrderArrayCh:
-			if localElevData, valInMap := localNodesInfo[localIP]; valInMap {
-				aliveNodesInfo := map[string]dt.ElevData{localIP: localElevData}
-				aliveNodesInfo[localIP] = localNodesInfo[localIP]
-				for _, nodeIP := range peerList.Peers {
-					if localNodesInfo[nodeIP] != (dt.ElevData{}) {
-						aliveNodesInfo[nodeIP] = localNodesInfo[nodeIP]
-					}
+		case allElevData[localIP] = <-localElevDataCh:
+		case hallRequests := <-HallRequestsCh:
+			aliveNodesElevData := []dt.NodeInfo{{IP: localIP, Data: allElevData[localIP]}}
+			for _, nodeIP := range peerList.Peers {
+				if nodeElevData, nodeElevDataSaved := allElevData[nodeIP]; nodeElevDataSaved {
+					aliveNodesElevData = append(aliveNodesElevData, dt.NodeInfo{IP: nodeIP, Data: nodeElevData})
 				}
-				costFuncInputSlice = dt.CostFuncInputSlice{
-					HallRequests: orders,
-					States:       dt.NodeInfoMapToSlice(aliveNodesInfo),
+			}
+			costFuncInputSlice = dt.CostFuncInputSlice{
+				HallRequests: hallRequests,
+				States:       aliveNodesElevData,
+			}
+			costFuncTimer.Reset(1)
+		case receivedData := <-receiveCh:
+			senderIP := receivedData.SenderIP
+			allNodesInfo := receivedData.AllNodeInfo
+
+			if senderIP == localIP {
+				break
+			}
+			for _, nodeInfo := range allNodesInfo {
+				if nodeInfo.IP == senderIP && !reflect.DeepEqual(allElevData[senderIP], nodeInfo.Data) {
+					allElevData[senderIP] = nodeInfo.Data
 				}
-				worldViewTimer.Reset(1)
 			}
 		case <-broadCastTimer.C:
-			//fmt.Printf("nodesInfo to NTW: %+v\n", dt.AllNodeInfoWithSenderIP{SenderIP: localIP, AllNodeInfo: dt.NodeInfoMapToSlice(localNodesInfo)})
-			transmittNodesInfo <- dt.AllNodeInfoWithSenderIP{SenderIP: localIP, AllNodeInfo: dt.NodeInfoMapToSlice(localNodesInfo)}
+			transmittCh <- dt.AllNodeInfoWithSenderIP{SenderIP: localIP, AllNodeInfo: dt.NodeInfoMapToSlice(allElevData)}
 			broadCastTimer.Reset(dt.BROADCAST_PERIOD)
 
-		case <-worldViewTimer.C:
+		case <-costFuncTimer.C:
 			select {
 			case costFuncInputCh <- costFuncInputSlice:
-
 			default:
-				worldViewTimer.Reset(1)
+				costFuncTimer.Reset(1)
 			}
 		}
 	}
 }
-
-// UNUSED allows unused variables to be included in Go programs
-func UNUSED(x ...interface{}) {}
