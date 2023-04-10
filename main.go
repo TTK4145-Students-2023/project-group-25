a563cdcba822db927030ad72dc2ffe1543476dd6
Fredrik Mo <fjmo@stud.ntnu.no>
2023-04-10 20:03:11 +0200

    write integration of elevator modules in main

--- main.go
+++ main.go
@@ -1,13 +1,97 @@
 package main
 
+// Import statements in alphabetic order
 import (
-	//elevtest "project/localElevator/elev_test"
-	intest "project/integrationTest"
+	localip "project/Network/Utilities/localip"
+	peers "project/Network/Utilities/peers"
+	masterSlaveNTW "project/Network/masterSlaveNTW"
+	btnassign "project/buttonAssigner"
+	dt "project/commonDataTypes"
+	elevDataDistributor "project/distributingSystem/elevDataDistributor"
+	orderStateHandler "project/distributingSystem/orderStateHandler"
+	elevio "project/localElevator/elev_driver"
+	elevfsm "project/localElevator/elev_fsm"
+	oassign "project/orderAssigner"
+	"time"
+)
+
+var (
+	masterSlaveRoleCh = make(chan dt.MasterSlaveRole)
+
+	costFuncInputCh              = make(chan dt.CostFuncInputSlice)
+	ordersFromMasterCh           = make(chan []dt.SlaveOrders)
+	ordersToSlavesCh             = make(chan []dt.SlaveOrders)
+	hallRequestsCh               = make(chan [dt.N_FLOORS][2]bool)
+	executedHallOrdersCh         = make(chan []elevio.ButtonEvent)
+	peerUpdate_MSCh              = make(chan peers.PeerUpdate)
+	peerUpdate_DataDistributorCh = make(chan peers.PeerUpdate)
+	peerUpdate_OrderHandlerCh    = make(chan peers.PeerUpdate)
+	peerTxEnableCh               = make(chan bool)
+
+	buttonEventCh     = make(chan elevio.ButtonEvent)
+	cabButtonEventCh  = make(chan elevio.ButtonEvent)
+	hallButtonEventCh = make(chan elevio.ButtonEvent)
+
+	floorCh    = make(chan int)
+	obstrCh    = make(chan bool)
+	elevDataCh = make(chan dt.ElevData)
+
+	hallOrderArrayCh = make(chan [dt.N_FLOORS][2]bool)
+
+	initCabRequestsCh = make(chan [dt.N_FLOORS]bool)
 )
 
 func main() {
+	localIP, _ := localip.LocalIP()
+	elevio.Init("localhost:15657", dt.N_FLOORS)
+	elevio.ClearAllLights()
 
-	//oassign.OrderAssignerTestFunc()
-	//elevtest.RunSingleElevTest()
-	intest.RunAssignerAndDistributionIntegrationTest()
+	go elevio.PollFloorSensor(floorCh)
+	go elevio.PollButtons(buttonEventCh)
+	go elevio.PollObstructionSwitch(obstrCh)
+
+	go peers.PeerListHandler(localIP,
+		peerTxEnableCh,
+		peerUpdate_MSCh,
+		peerUpdate_DataDistributorCh,
+		peerUpdate_OrderHandlerCh)
+
+	go masterSlaveNTW.MasterSlaveNTW(localIP,
+		peerUpdate_MSCh,
+		ordersToSlavesCh,
+		ordersFromMasterCh,
+		masterSlaveRoleCh)
+
+	go oassign.OrderAssigner(localIP,
+		masterSlaveRoleCh,
+		costFuncInputCh,
+		ordersFromMasterCh,
+		ordersToSlavesCh,
+		hallRequestsCh)
+
+	go elevDataDistributor.DataDistributor(localIP,
+		elevDataCh,
+		hallOrderArrayCh,
+		costFuncInputCh,
+		peerUpdate_DataDistributorCh,
+		initCabRequestsCh)
+
+	go orderStateHandler.OrderStateHandler(localIP,
+		hallButtonEventCh,
+		executedHallOrdersCh,
+		hallOrderArrayCh,
+		peerUpdate_OrderHandlerCh)
+
+	time.Sleep(time.Millisecond * 40)
+
+	go elevfsm.FSM(hallRequestsCh,
+		cabButtonEventCh,
+		floorCh,
+		obstrCh,
+		elevDataCh,
+		executedHallOrdersCh,
+		initCabRequestsCh,
+		peerTxEnableCh)
+
+	btnassign.ButtonHandler(buttonEventCh, hallButtonEventCh, cabButtonEventCh)
 }
